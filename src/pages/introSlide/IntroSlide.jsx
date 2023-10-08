c7aab847b5d409791f49f25ce8767adc9b5a4dfc
XaviAlvarado18 <alv21188@uvg.edu.gt>
2023-10-07 18:22:41 -0600

    IntroSlide

--- src/pages/introSlide/IntroSlide.jsx
+++ src/pages/introSlide/IntroSlide.jsx
@@ -5,6 +5,12 @@ import { principal } from './IntroSlide.module.scss'
 const IntroSlide = ({ goNext }) => (
   <div className={principal}>
     <h1>Everything Starts With Water</h1>
+    <p>Explora el fascinante ciclo del agua y 
+      reflexiona sobre como el cambio climatico 
+      esta desafiando la manera en que interactuamos 
+      con este recurso vital, remodelando nuestro entendimiento
+      y exigiendo una gestion mas cuidadosa y sostenible 
+      para garantizar su disponibilidad en el futuro.</p>
     <button onClick={goNext} type="button">
       Hola
     </button>
