43974f822f703780d69cd64fd2af1fb1bbb57072
Daniel Armando Valdez Reyes <77825594+Danval-003@users.noreply.github.com>
2023-10-08 14:13:25 -0600

    Styles

--- src/pages/WorldSlide/WorldSlide.jsx
+++ src/pages/WorldSlide/WorldSlide.jsx
@@ -7,13 +7,14 @@ import {
 
 import Earth from '../../assets/earth.gif'
 
-const WorldSlide = ({ goNext, texts }) => (
+const WorldSlide = ({ goNext, texts, goReturn }) => (
   <div>
     <div className={principal}>
       <h1>{texts[0]}</h1>
       <p className={secondary}>
         {texts[1]}
       </p>
+      <RoundedButton onClick={goReturn} text={texts[3]} />
       <RoundedButton onClick={goNext} text={texts[2]} />
     </div>
     <img src={Earth} alt="JAksjdk" className={world} />
@@ -23,11 +24,13 @@ const WorldSlide = ({ goNext, texts }) => (
 
 WorldSlide.propTypes = {
   goNext: PropTypes.func,
+  goReturn: PropTypes.func,
   texts: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 WorldSlide.defaultProps = {
   goNext: null,
+  goReturn: null,
 }
 
 export default WorldSlide
