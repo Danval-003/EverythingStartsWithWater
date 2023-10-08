7597e94273366cd8f616f0b59312e25311acf48b
Daniel Armando Valdez Reyes <77825594+Danval-003@users.noreply.github.com>
2023-10-07 18:31:25 -0600

    Intro con fuente

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ import { Espanol } from '@text'
 
 const App = () => {
   const [index, setIndex] = useState(0)
-  const [percentage, setPercentage] = useState(0.0)
+  const [percentage, setPercentage] = useState(1.0)
 
   return (
     <div>
       <InteractiveWater percentaje={percentage} />
       <Slider currentIndex={index}>
-        <IntroSlide goNext={() => { setIndex(0); setPercentage(1.0) }} texts={Espanol.Slide1} />
-        <IntroductionSlide goNext={() => { setIndex(1); setPercentage(0.80) }} />
+        <IntroSlide goNext={() => { setIndex(1); setPercentage(0.9) }} texts={Espanol.Slide1} />
+        <IntroductionSlide goNext={() => { setIndex(0); setPercentage(1.0) }} />
       </Slider>
     </div>
   )
